9a093ef40ff696fc613e8bf828ed65a37089942c
Kimberlly Hapuque <141903994+kimhapuque@users.noreply.github.com>
2024-01-30 10:32:29 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,32 @@
+alert("Jogo do Acerte o número");
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1)
+console.log (numeroSecreto);
+let chute;
+let tentativas = 1;
+
+//enquanto o chute for diferente do numero secre
+while (chute != numeroSecreto) {
+    chute  = prompt (`Chute um número entre 1 e ${numeroMaximo}:`);
+    // se o chute for igual ao numero secreto:
+if (numeroSecreto == chute) {
+break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert (`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = (tentativas + 1)
+        tentativas++;
+    } // senao 
+    }
+    //essa palhaçada toda aqui significa oq ta comentado em baixo
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+    alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas} ${palavraTentativa}.`);
+        // if (tentativas > 1) {
+        //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou de primera`);
+        //     }
+        // else (            
+        //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas}`));
+            
---
+++ logica-js-projeto_inicial/tarefa.js
@@ -0,0 +1,25 @@
+alert("Boas vindas ao nosso site!");
+
+var nome = "Lua";
+
+var idade = 25;
+
+var numeroDeVendas = 50;
+
+var saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+var mensagemDeErro = "Erro! Preencha todos os campos";
+
+alert(mensagemDeErro);
+
+nome = prompt("Digite seu nome:");
+
+idade = prompt("Digite sua idade:");
+
+idade = parseInt(idade);
+
+if (idade >= 18) {
+  alert("Pode tirar a habilitação!");
+}
