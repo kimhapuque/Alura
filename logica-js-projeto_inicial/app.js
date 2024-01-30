12368eae40d96d82fc28ed3b8d75f48b9ea22f3b
WesleyRSC <wesley8rogerio@gmail.com>
2024-01-30 14:56:48 -0300

    teste de troca de telas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,32 +1,46 @@
-alert("Jogo do Acerte o número");
-let numeroMaximo = 100;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1)
-console.log (numeroSecreto);
-let chute;
-let tentativas = 1;
+function startGame() {
+  console.log("Iniciou o jogo");
 
-//enquanto o chute for diferente do numero secre
-while (chute != numeroSecreto) {
-    chute  = prompt (`Chute um número entre 1 e ${numeroMaximo}:`);
-    // se o chute for igual ao numero secreto:
-if (numeroSecreto == chute) {
-break;
-    } else {
+  document.getElementById("start-screen").hidden = true;
+  document.getElementById("success-screen").hidden = false;
+
+  runGameLogic()
+}
+
+function runGameLogic(){
+
+  if (false) {
+    let numeroMaximo = 100;
+    let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+    console.log(numeroSecreto);
+    let chute;
+    let tentativas = 1;
+
+    //enquanto o chute for diferente do numero secre
+    while (chute != numeroSecreto) {
+      chute = prompt(`Chute um número entre 1 e ${numeroMaximo}:`);
+      // se o chute for igual ao numero secreto:
+      if (numeroSecreto == chute) {
+        break;
+      } else {
         if (chute > numeroSecreto) {
-            alert (`O número secreto é menor que ${chute}`);
+          alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert (`O número secreto é maior que ${chute}`);
+          alert(`O número secreto é maior que ${chute}`);
         }
         //tentativas = (tentativas + 1)
         tentativas++;
-    } // senao 
+      } // senao
     }
     //essa palhaçada toda aqui significa oq ta comentado em baixo
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-    alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas} ${palavraTentativa}.`);
-        // if (tentativas > 1) {
-        //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou de primera`);
-        //     }
-        // else (            
-        //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas}`));
-            
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    alert(
+      `arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas} ${palavraTentativa}.`
+    );
+    // if (tentativas > 1) {
+    //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou de primera`);
+    //     }
+    // else (
+    //     alert (`arrazou amg, o numero era ${numeroSecreto} e você acertou com ${tentativas}`));
+  }
+}
